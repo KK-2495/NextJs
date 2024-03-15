e170e98cfc817bfe14db1a81f0010f0ce931c933
Krishnan Kalimuthu <krishnankalimuthunadar@hotmail.com>
2024-03-15 21:08:56 +0530

    module css basics

--- next-app/app/users/contact/page.jsx
+++ next-app/app/users/contact/page.jsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 const Contact = () => {
   return (
     <>
-      <Link href="/users/content">
-        <h1>This is my About Component</h1>
-        <button className=" p-1 border border-blue-600 bg-white text-black ">
-          Click Me to Proceed
-        </button>
-      </Link>
+      <div className="flex min-h-screen flex-col items-center justify-between text-center p-24">
+        <Link href="/users/content">
+          <h1>This is my About Component</h1>
+          <button className=" p-1 border border-blue-600 bg-white text-black ">
+            Click Me to Proceed
+          </button>
+        </Link>
+      </div>
     </>
   );
 };
--- next-app/app/users/content/page.jsx
+++ next-app/app/users/content/page.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const NextApp = () => {
   return (
     <>
-      <div>
+      <div className="flex flex-col items-center justify-between text-center p-24">
         This is my First Next App <br />
         <Link href="/">
           <button className="p-3 text-center bg-slate-500 rounded-xl">
